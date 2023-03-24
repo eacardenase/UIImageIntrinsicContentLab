2367f36e6f3fd938872ed0497c7663a9f4b162df
Edwin Cardenas <eacardenase@unal.edu.co>
2023-03-24 11:58:38 -0500

    feat: add chcr to ui image

--- UIImageIntrinsicContentLab/ViewController.swift
+++ UIImageIntrinsicContentLab/ViewController.swift
@@ -11,9 +11,81 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupViews()
     }
-
-
 }
 
+extension ViewController {
+    func setupViews() {
+        let image = makeImageView(named: "rush")
+        let label = makeLabel(withText: "Title")
+        let button = makeButton(withText: "Get Started")
+        
+        view.addSubview(image)
+        view.addSubview(label)
+        view.addSubview(button)
+        
+        // imageView by themselves have intrinsic content size
+        NSLayoutConstraint.activate([
+            image.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            image.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            image.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
+        
+        // label
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: image.bottomAnchor, constant: 8),
+            label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8),
+            label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -8)
+        ])
+        
+        // button
+        NSLayoutConstraint.activate([
+            button.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 300),
+            button.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16),
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+    
+    func makeImageView(named: String) -> UIImageView {
+        let view = UIImageView()
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.contentMode = .scaleAspectFit
+        view.image = UIImage(named: named)
+        
+        // why only one dimension? because height is the only dimension ambigous
+        view.setContentHuggingPriority(UILayoutPriority.defaultLow, for: .vertical)
+        view.setContentCompressionResistancePriority(UILayoutPriority.defaultLow, for: .vertical)
+        
+        return view
+    }
+    
+    func makeLabel(withText text: String) -> UILabel {
+        let label = UILabel()
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = text
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 32)
+        label.backgroundColor = .yellow
+        
+        return label
+    }
+    
+    func makeButton(withText text: String) -> UIButton {
+        let button = UIButton()
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle(text, for: .normal)
+        
+        var config = UIButton.Configuration.filled()
+        config.contentInsets = NSDirectionalEdgeInsets(top: 16, leading: 16, bottom: 16, trailing: 16)
+        config.cornerStyle = .capsule
+        
+        button.configuration = config
+        
+        return button
+    }
+}
